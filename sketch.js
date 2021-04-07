059b6249c531bf20d1528a29725dbd7b8cad4882
nhhg <81352618+nhhg@users.noreply.github.com>
2021-04-07 13:55:48 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+var a,b;
+
+
+function setup() {
+  createCanvas(800,400);
+  a = createSprite(400, 200, 50, 80);
+  a.shapeColor="green";
+
+  b = createSprite(400, 200, 80, 50);
+  b.shapeColor="green";
+
+
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+  a.y= World.mouseY;
+  b.x= World.mouseX;
+
+
+if(a.x - b.x < a.width/2 + b.width/2 && b.x- a.x < a.width/2 + b.width/2
+  && a.y - b.y < a.height/2 + b.height/2 && b.y - a.y < a.height/2 + b.height/2){
+a.shapeColor = "red";
+b.shapeColor = "red";
+}
+
+else{
+
+  a.shapeColor = "green";
+  b.shapeColor = "green";
+
+}
+}
